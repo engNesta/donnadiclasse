fb81ca8c74406e833699e975aad291eff4c49474
engNesta <124577220+engNesta@users.noreply.github.com>
2024-11-26 16:49:02 +0100

    new placement text

--- src/pages/Index.tsx
+++ src/pages/Index.tsx
@@ -35,28 +35,6 @@ const Index = () => {
           </div>
         </div>
 
-        <div className="px-4 py-16 bg-transparent">
-          <div className="max-w-3xl mx-auto text-center space-y-6 text-rich-black">
-            <p className="text-lg leading-relaxed">
-              Vad trevligt att du har hittat till vår underbara lilla butik! Här nedan kan du klicka dig vidare till vårt breda produktutbud av högkvalitativt, elegant och glamoröst dammode för alla åldrar, former och tillställningar.
-            </p>
-            <p className="text-lg leading-relaxed">
-              Har du några frågor? Tveka inte att höra av dig till oss så hjälper vi dig att hitta det perfekta plagget för just dig.
-            </p>
-            <p className="text-lg leading-relaxed">
-              Varmt välkommen in till oss på Västra Storgatan 32 i Kristianstad för optimerad personlig service!
-            </p>
-
-            <div className="flex flex-col sm:flex-row gap-4 justify-center mt-12">
-              <Link to="/products" className="btn-primary">
-                Explore Products
-              </Link>
-              <Link to="/contact" className="btn-secondary">
-                Contact Us
-              </Link>
-            </div>
-          </div>
-        </div>
       </main>
 
       <Footer />
